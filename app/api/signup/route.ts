1575676fc4b1f4eefa6696f85aabcf429929db1c
Mounas82 <abdulrehman03082007@gmail.com>
2024-09-11 23:12:03 +0500

    route updated

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -4,11 +4,11 @@ import { User } from "@/models/userModel"
 dbConnect()
 
 export async function POST(req: any) {
-    // const body = await req.json()
-    // const exists = await User.findOne({ "user.email": body.email })
-    // if (exists) {
-    //     return Response.json({ message: "error" })
-    // }
-    // const user = await User.create({ user: body, todos: [] })
+    const body = await req.json()
+    const exists = await User.findOne({ "user.email": body.email })
+    if (exists) {
+        return Response.json({ message: "error" })
+    }
+    const user = await User.create({ user: body, todos: [] })
     return Response.json({ message: "success" })
 }
