94c86a993583829a9ed27630018c7c4f76a252bf
Mounas82 <abdulrehman03082007@gmail.com>
2024-09-14 22:31:58 +0500

    middleware updated

--- middleware.ts
+++ middleware.ts
@@ -2,13 +2,10 @@ import { getToken } from 'next-auth/jwt';
 import { NextResponse } from 'next/server';
 
 export async function middleware(req:any) {
-  const secret = process.env.NEXTAUTH_SECRET;
-
-  const token = await getToken({ req, secret });
-  console.log(token);
-  
-  
-  if (!token) {
+  const localCookie = req.cookies.has("__Secure-next-auth.session-token")
+  const vercelCookie = req.cookies.has("next-auth.session-token")
+  const isLogin = localCookie || vercelCookie
+  if (!isLogin) {
     return NextResponse.redirect(new URL('/Login', req.url));
   }
   return NextResponse.next();
